717e812cf8f6f45e43bc454a3268871f5d0a4058
Mohdkumailabbas <kumailabbasbbk@gmail.com>
2024-10-10 12:18:02 +0530

    done with setting up middleware & route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,34 @@
-
+import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
- 
+ export { default } from "next-auth/middleware"
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  const path =request.nextUrl.pathname
-  const publicPath= path ==="/log-in"|| path ==="/sign-up"
-  const token =request.cookies.get('token')?.value||""
-  if(publicPath && token){
-     return NextResponse.redirect(new URL("/",request.nextUrl))
+export async function middleware(request: NextRequest) {
+  const token= await getToken({req : request})
+  const url =request.nextUrl
+//   const publicPath= path ==="/log-in"|| path ==="/sign-up"
+//   const token =request.cookies.get('token')?.value||""
+//   if(publicPath && token){
+//      return NextResponse.redirect(new URL("/",request.nextUrl))
+//   }
+//   if(!publicPath && !token){
+//     return NextResponse.redirect(new URL("/login",request.nextUrl))
+//  }
+  if(token && (
+    url.pathname.startsWith('sign-in')
+
+  )){
+    return NextResponse.redirect(new URL("/",request.url))
   }
-  if(!publicPath && !token){
-    return NextResponse.redirect(new URL("/login",request.nextUrl))
- }
+
 }
  
 // See "Matching Paths" below to learn more
 export const config = {
   matcher: [
-    '/log-in',
-    '/sign-up',
-    '/'
+    // '/log-in',
+    // '/sign-up',
+    // '/',
+    '/sign-in'
   ],
 }
